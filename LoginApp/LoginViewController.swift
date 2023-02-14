d5ca29c77840b8afb7d6b7136d49ee24f1749ffb
Tika Dzamikh <dzamikh@protonmail.com>
2023-02-14 23:33:54 +0300

    Work with logic

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Timur Dzamikh on 14.02.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userNameTF.text
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func logInButtonTapped() {
+        guard userNameTF.text == "One", passwordTF.text == "1" else {
+            showAlert(withTitle: "Invalid login or password", andMessage: "Please, enter correct login and password")
+            return
+        }
+    }
+        
+    @IBAction func forgotUNButtonTapped() {
+        showAlert(withTitle: "Ooops!", andMessage: "Your name is One")
+    }
+    
+    @IBAction func forgotPassButtonTapped() {
+        showAlert(withTitle: "Ooops!", andMessage: "Your password is 1")
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTF.text = nil
+        passwordTF.text = nil
+    }
+}
+
+extension LoginViewController {
+    private func showAlert (withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTF.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Timur Dzamikh on 14.02.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var userName: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        welcomeLabel.text = "Welcome, \(userName ?? "")"
+    }
+    
+    @IBAction func logOutButtonTapped() {
+        dismiss(animated: true)
+    }
+    
+}
