d1fa82467210094b397defcbbbcf3657756ba0ac
Tika Dzamikh <dzamikh@protonmail.com>
2023-02-15 01:15:36 +0300

    Bug fixes

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -16,10 +16,23 @@ final class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         welcomeLabel.text = "Welcome, \(userName ?? "")!"
+        gradientColor()
     }
     
     @IBAction func logOutButtonTapped() {
         dismiss(animated: true)
     }
     
+    private func gradientColor () {
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = [
+            UIColor.systemPink.cgColor,
+            UIColor.systemIndigo.cgColor,
+        ]
+        gradient.locations = [0, 1]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        view.layer.insertSublayer(gradient, at: 0)
+    }
 }
